756c9aba8fd2747af5c67a6d79253375f28573b1
wld <d1zciplineo@gmail.com>
2024-04-15 07:50:32 +0300

    Added interactive map

--- js/script.js
+++ js/script.js
@@ -6,9 +6,51 @@ function init() {
 		zoom: 11,
 	})
 
-	let placemark = new ymaps.Placemark(
+	let placemark_avtozavodskaya = new ymaps.Placemark(
 		[55.7043329792479, 37.64653060633471],
-		{},
+		{
+			balloonContent: `
+        <div class="map__body">
+          <div class="map__header">Автозаводская</div>
+          <div class="map__address">Автозаводская улица, 16с5</div>
+          <div class="map__contacts">
+            <a href="tel: +79000000000">+79000000000</a>
+          </div>
+        </div>
+      `,
+		},
+		{}
+	)
+
+	let placemark_zuzino = new ymaps.Placemark(
+		[55.65527954179762, 37.57239166695404],
+		{
+			balloonContent: `
+        <div class="map__body">
+          <div class="map__header">Зюзино</div>
+          <div class="map__address">Москва, улица Каховка, 27</div>
+          <div class="map__contacts">
+            <a href="tel: +79000000000">+79000000000</a>
+          </div>
+        </div>
+      `,
+		},
+		{}
+	)
+
+	let placemark_seligerskaya = new ymaps.Placemark(
+		[55.863668907556416, 37.546911805178645],
+		{
+			balloonContent: `
+        <div class="map__body">
+          <div class="map__header">Cелигерская</div>
+          <div class="map__address">Москва, Дмитровское шоссе, 89</div>
+          <div class="map__contacts">
+            <a href="tel: +79000000000">+79000000000</a>
+          </div>
+        </div>
+      `,
+		},
 		{}
 	)
 
@@ -19,7 +61,9 @@ function init() {
 	map.controls.remove('fullscreenControl') // удаляем кнопку перехода в полноэкранный режим
 	map.controls.remove('rulerControl') // удаляем контрол правил
 
-	map.geoObjects.add(placemark)
+	map.geoObjects.add(placemark_avtozavodskaya)
+	map.geoObjects.add(placemark_zuzino)
+	map.geoObjects.add(placemark_seligerskaya)
 }
 
 ymaps.ready(init)
